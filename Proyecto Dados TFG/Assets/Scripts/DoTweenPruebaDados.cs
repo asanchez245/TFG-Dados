74312cbdd5b46bb0b1fa2c83debcb1867d4eede3
mmcc7 <marietacidfandos509@gmail.com>
2025-03-05 00:32:47 +0100

    PruebaDoTween

--- Proyecto Dados TFG/Assets/Scripts/DoTweenPruebaDados.cs
+++ Proyecto Dados TFG/Assets/Scripts/DoTweenPruebaDados.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
 
 public class DoTweenPruebaDados : MonoBehaviour
 {
+    
+    public GameObject dotweentestObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -9,8 +14,35 @@ public class DoTweenPruebaDados : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+ 
+    public void DoTweenTestFunction()
+    { 
+        //dotweentestObject.transform.DOScaleX(dotweentestObject.transform.localScale.x + 4, 1.25f).SetEase(Ease.OutBounce).OnComplete(() => 
+        //{
+        //    dotweentestObject.transform.DOMoveY(dotweentestObject.transform.position.y + 2, 1.5f);
+        //});
+
+        Sequence secuenciaTest = DOTween.Sequence();
+
+        secuenciaTest.Append(dotweentestObject.transform.DOMoveX(dotweentestObject.transform.position.x + 6, 1.25f).SetEase(Ease.OutBounce))
+           /* .Join(dotweentestObject.transform.DOMoveX(dotweentestObject.transform.position.x - 6, 1.5f))*/;
+            //.Join(dotweentestObject.GetComponent<SpriteRenderer>().DOColor(Color.green, 2));
+
+
+        //secuenciaTest.Append(dotweentestObject.transform.DOLocalRotate(new Vector3(100, 100, 100), 3).OnComplete(() =>
+        //{
+        //    Destroy(dotweentestObject);
+        //}));
+
+
+        //append -> hace los tween en orden
+        //join -> hace los tween al mismo tiempo
+
+    }
+    public void ExitButton()
     {
-        
+        Sequence secuenciaTest = DOTween.Sequence();
+
+        secuenciaTest.Append(dotweentestObject.transform.DOMoveX(dotweentestObject.transform.position.x - 6, 1.25f).SetEase(Ease.OutBounce));
     }
 }
